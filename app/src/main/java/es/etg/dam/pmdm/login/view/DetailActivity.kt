1112773c532b6ca6f6785457f3cd6d0da0907827
liuDam1 <liu.jiangyu.dam@gmail.com>
2025-11-08 02:24:47 +0100

    completo

--- app/src/main/java/es/etg/dam/pmdm/login/view/DetailActivity.kt
+++ app/src/main/java/es/etg/dam/pmdm/login/view/DetailActivity.kt
@@ -1,10 +1,11 @@
 package es.etg.dam.pmdm.login.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import es.etg.dam.pmdm.login.R
 
 class DetailActivity : AppCompatActivity() {
@@ -12,5 +13,17 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
+        val nombreTextView = findViewById<TextView>(R.id.txtNombre)
+        val extras = intent.extras
+
+        if (extras != null) {
+            val nombre = extras.getString(LoginActivity.Constantes.NOMBRE, "")
+            nombreTextView.text = nombre
+        }
+    }
+
+    fun volver(view : View){
+        val intent = Intent(this, LoginActivity::class.java);
+        startActivity(intent);
     }
 }
--- app/src/main/java/es/etg/dam/pmdm/login/view/LoginActivity.kt
+++ app/src/main/java/es/etg/dam/pmdm/login/view/LoginActivity.kt
@@ -1,24 +1,45 @@
 package es.etg.dam.pmdm.login.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
+import android.widget.EditText  // ¡Cambiado de TextView a EditText!
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import es.etg.dam.pmdm.login.R
+import es.etg.dam.pmdm.login.view.LoginActivity.Constantes.NOMBRE
 
 class LoginActivity : AppCompatActivity() {
+    object Constantes {
+        const val NOMBRE: String = "Nombre"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        val nombre = leer()
+        if (nombre != null && nombre.isNotEmpty()) {
+            findViewById<EditText>(R.id.txtNombre).setText(nombre)
+        }
     }
 
-    fun guardar(view: View){
-        val nombre = findViewById<TextView>(R.id.txtNombre).text
+    fun guardar(view: View) {
+        val nombre = findViewById<EditText>(R.id.txtNombre).text.toString()
         val sharedPref = getPreferences(MODE_PRIVATE)
         val editor = sharedPref.edit()
-        editor.putString("nombre", nombre.toString())
+        editor.putString(NOMBRE, nombre)
         editor.apply()
+
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(NOMBRE, nombre)
+        startActivity(intent)
+    }
+
+    private fun leer(): String? {
+        val sharedPref = getPreferences(MODE_PRIVATE)
+        val nombre = sharedPref.getString(NOMBRE, "")
+        return nombre
     }
 }
